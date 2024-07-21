4d0f3a4e8761361106adca5fe3303937e0e3eca1
Deepakraj Teekaraman <deepakrajteekaraman@gmail.com>
2024-07-21 16:34:23 +0200

    variables

---
+++ Python_Learning.py
@@ -0,0 +1,42 @@
+'''
+#Hello world
+print ('Hello world!')
+
+'''
+
+'''
+#Variable is a container for value. Behave as a value that contains
+first_name = "Deepakraj"
+last_name = "Teekaraman"
+full_name = first_name +" "+ last_name
+print(type(full_name))   
+print ("Hello "+full_name)
+'''
+
+'''
+#integer (Not assign value within quotes, technically it will become string, can only concanate string with string so use type change)
+age = 24
+age += 1
+print (type(age))
+print ("your age is: "+ str(age))
+
+#float
+height = (280.55)
+print (height)
+print (type(height))
+print ("Your height is: "+str(height)+" cm")
+
+#boolean
+human = False
+print (type(human))
+print(human)
+'''
+'''
+#Multiple assignments in python. This will help in assigning multiple variables to one line of code at same time
+name, age, height = 'deepakraj', 24, 245.55
+print(name+" "+str(age)+" "+str(height))
+'''
+
+#String methods in python
+ 
+
